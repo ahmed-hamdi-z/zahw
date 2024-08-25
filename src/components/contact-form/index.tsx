dc1babd1f8e79c36e9dcf2c8ce7fd0a995b39f1e
Ahmed Zedan <ahmedhamdizedan@gmail.com>
2024-08-25 13:22:26 +0300

    update contact form

--- src/components/contact-form/index.tsx
+++ src/components/contact-form/index.tsx
@@ -44,16 +44,16 @@ const ContactForm: React.FC = () => {
   return (
     <>
       <button
-        className="fixed bottom-4 right-4 bg-[#764095]  text-white font-bold w-14 h-14 px-[11px] rounded-full focus:outline-none focus:shadow-outline"
+        className="fixed bottom-4 right-4 bg-[#764095] text-white font-bold w-14 h-14 px-[11px] rounded-full focus:outline-none focus:shadow-outline"
         onClick={toggleForm}
       >
         <TiMessages  className='w-8 h-8'/>
       </button>
 
       {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-end z-50">
+        <div className="fixed inset-0 flex items-center justify-end rtl:justify-end z-50 ">
           <div className="fixed inset-0 bg-opacity-50 " onClick={toggleForm}></div>
-          <div className="relative right-3 bg-white shadow-lg p-2 md:w-1/4 w-5/6 md:h-[70vh] h-auto z-50  " onClick={(e) => e.stopPropagation()}>
+          <div className="relative right-5 bg-white shadow-md shadow-[#764095] p-2 md:w-1/4 w-5/6 md:h-[70vh] h-auto z-50  " onClick={(e) => e.stopPropagation()}>
             <h2 className="text-2xl mb-6 text-center text-[#764095]">{t("Welcome To Zahw")}</h2>
             <p className="mb-6 text-center text-gray-600">{t("submit form")}</p>
             <form ref={form} onSubmit={sendEmail} className="w-full ">
@@ -64,7 +64,7 @@ const ContactForm: React.FC = () => {
                   name="user_name"
                   type="text"
                   placeholder={t("full name")}
-                  className="w-full  border-[1px] border-slate-300 px-2.5 py-1.5 focus:outline-[#764095]"
+                  className="w-full  border-[1px] border-slate-300 px-2.5 py-1.5 focus:outline-[#764095] shadow-md shadow-[#764095]"
                   required
                 />
               </motion.div>
@@ -76,7 +76,7 @@ const ContactForm: React.FC = () => {
                   name="user_email"
                   type="email"
                   placeholder={t("ُEmail")}
-                  className="w-full  border-[1px] border-slate-300 px-2.5 py-1.5 focus:outline-[#764095]"
+                  className="w-full  border-[1px] border-slate-300 px-2.5 py-1.5 focus:outline-[#764095] shadow-md shadow-[#764095]"
                   required
                 />
               </motion.div>
@@ -87,7 +87,7 @@ const ContactForm: React.FC = () => {
                   id="Mobile-number-input"
                   type="number"
                   placeholder={t("phone number")}
-                  className="w-full  border-[1px] border-slate-300 px-2.5 py-1.5 focus:outline-[#764095]"
+                  className="w-full  border-[1px] border-slate-300 px-2.5 py-1.5 focus:outline-[#764095] no-spin shadow-md shadow-[#764095]"
                   required
                 />
 
@@ -97,7 +97,7 @@ const ContactForm: React.FC = () => {
                   name="message"
                   type="text"
                   placeholder={t("Enter Your Message")}
-                  className="w-full  border-[1px] border-slate-300 px-2.5 py-1.5 focus:outline-[#764095] mt-2 h-20 placeholder:text-ellipsis"
+                  className="w-full  border-[1px] border-slate-300 px-2.5 py-1.5 focus:outline-[#764095] mt-2 h-20 placeholder:text-ellipsis shadow-md shadow-[#764095]"
                   required
                 />
               </motion.div>
@@ -107,12 +107,12 @@ const ContactForm: React.FC = () => {
               <input
                 value={t("Send")}
                 type="submit"
-                className="py-1 px-2  bg-[#764095]  text-center font-medium text-white transition-colors hover:bg-[#764095]"
+                className="py-1 px-2  bg-[#764095]  text-center font-medium text-white transition-colors hover:bg-[#764095] shadow-md shadow-[#764095]"
               />
             </form>
             {message && (
           <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-             <div className="bg-white text-black p-6 shadow-lg w-96 h-32 flex flex-col items-center justify-center">
+             <div className="bg-white text-black p-6 shadow-lg shadow-[#764095] w-96 h-32 flex flex-col items-center justify-center">
               <button
                 onClick={() => setMessage(null)}
                 className="relative left-40 top-0 mb-4 font-bold  w-7 h-7 text-black text-3xl z-50"
