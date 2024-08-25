62f0b2c99ff4a76d89ca49d4583babbb3650fc38
Ahmed Zedan <ahmedhamdizedan@gmail.com>
2024-08-26 02:57:06 +0300

    update clients

--- src/components/clients/index.tsx
+++ src/components/clients/index.tsx
@@ -1,59 +1,63 @@
-import { FC, useState, useEffect } from "react";
 import { motion } from "framer-motion";
-import HeadingSeparator from "../heading";
-import { t } from "i18next";
 import imagesData from "./images.json";
+import { useTranslation } from "react-i18next";
+import HeadingSeparator from "../heading";
 
-const Clients: FC = () => {
+const Clients = () => {
+  const { t } = useTranslation();
   return (
-    <section
-      className="bg-white p-3 mt-16 md:w-9/12 w-full flex flex-col"
-      id="clients"
-    >
+    <section className="bg-white py-4" id="clients">
       <HeadingSeparator title={t("Clients")} />
-      <div className="flex overflow-hidden mt-4">
+      <div className="flex overflow-hidden">
         <TranslateWrapper>
-          <LogoItem />
+          {imagesData.images.map((image, index) => (
+            <LogoItem key={index} Icon={image} />
+          ))}
+        </TranslateWrapper>
+        <TranslateWrapper>
+          {imagesData.images.map((image, index) => (
+            <LogoItem key={index} Icon={image} />
+          ))}
+        </TranslateWrapper>
+        <TranslateWrapper>
+          {imagesData.images.map((image, index) => (
+            <LogoItem key={index} Icon={image} />
+          ))}
         </TranslateWrapper>
       </div>
     </section>
   );
 };
 
-const TranslateWrapper = ({ children }: { children: JSX.Element }) => {
+const TranslateWrapper = ({
+  children,
+  reverse,
+}: {
+  children: JSX.Element | JSX.Element[];
+  reverse?: boolean;
+}) => {
   return (
     <motion.div
-      initial={{ translateX: "0%" }}
-      animate={{ translateX: "-50%" }}
-      transition={{ duration: 20, repeat: Infinity, ease: "linear" }}
+      initial={{ translateX: reverse ? "-100%" : "0%" }}
+      animate={{ translateX: reverse ? "0%" : "-100%" }}
+      transition={{ duration: 10, repeat: Infinity, ease: "linear" }}
       className="flex gap-4 px-2"
-      style={{ width: "200%" }} // double the width to accommodate two sets of images
     >
       {children}
-
     </motion.div>
   );
 };
 
-const LogoItem = () => {
-  const [images, setImages] = useState<string[]>([]);
-
-  useEffect(() => {
-    setImages(imagesData.images);
-  }, []);
-
+const LogoItem = ({ Icon }: { Icon: string }) => {
   return (
-    <div className="flex gap-4">
-      {images.map((image, i) => (
-        <img
-          key={i}
-          src={image}
-          className="w-20 h-20 ml-3"
-          alt={`Gallery item ${i}`}
-          loading="lazy"
-        />
-      ))}
-    </div>
+    <a
+      href="/"
+      rel="nofollow"
+      target="_blank"
+      className="w-16 md:w-24 h-16 md:h-24 flex justify-center items-center hover:bg-slate-200 text-black transition-colors"
+    >
+      <img src={Icon} alt="Logo" className="w-full h-full object-contain" />
+    </a>
   );
 };
 
--- src/components/contact-form/index.tsx
+++ src/components/contact-form/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
@@ -24,8 +24,7 @@ const ContactForm: React.FC = () => {
         )
         .then(
           () => {
-         
-            if (form.current) { 
+            if (form.current) {
               setMessage(t("تم تسجيل ردكم بنجاح"));
               form.current.reset(); // Reset the form after successful submission
             }
@@ -47,18 +46,25 @@ const ContactForm: React.FC = () => {
         className="fixed bottom-4 right-4 bg-[#764095] text-white font-bold w-14 h-14 px-[11px] rounded-full focus:outline-none focus:shadow-outline"
         onClick={toggleForm}
       >
-        <TiMessages  className='w-8 h-8'/>
+        <TiMessages className="w-8 h-8" />
       </button>
 
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-end rtl:justify-start z-50 ">
-          <div className="fixed inset-0 bg-opacity-50 " onClick={toggleForm}></div>
-          <div className="relative right-5 bg-white shadow-md shadow-[#764095] p-2 md:w-1/4 w-5/6 md:h-[70vh] h-auto z-50  " onClick={(e) => e.stopPropagation()}>
-            <h2 className="text-2xl mb-6 text-center text-[#764095]">{t("Welcome To Zahw")}</h2>
+          <div
+            className="fixed inset-0 bg-opacity-50 "
+            onClick={toggleForm}
+          ></div>
+          <div
+            className="relative right-5 bg-white shadow-md shadow-[#764095] p-2 md:w-1/4 w-5/6 md:h-[70vh] h-auto z-50  "
+            onClick={(e) => e.stopPropagation()}
+          >
+            <h2 className="text-2xl mb-6 text-center text-[#764095]">
+              {t("Welcome To Zahw")}
+            </h2>
             <p className="mb-6 text-center text-gray-600">{t("submit form")}</p>
             <form ref={form} onSubmit={sendEmail} className="w-full ">
-              <motion.div  className="mb-2 w-full">
-               
+              <motion.div className="mb-2 w-full">
                 <input
                   id="name-input"
                   name="user_name"
@@ -70,7 +76,6 @@ const ContactForm: React.FC = () => {
               </motion.div>
 
               <motion.div variants={primaryVariants} className="mb-2 w-full">
-             
                 <input
                   id="email-input"
                   name="user_email"
@@ -82,7 +87,6 @@ const ContactForm: React.FC = () => {
               </motion.div>
 
               <motion.div variants={primaryVariants} className="mb-4 w-full">
-               
                 <input
                   id="Mobile-number-input"
                   type="number"
@@ -91,7 +95,6 @@ const ContactForm: React.FC = () => {
                   required
                 />
 
-                
                 <input
                   id="message"
                   name="message"
@@ -102,7 +105,10 @@ const ContactForm: React.FC = () => {
                 />
               </motion.div>
 
-              <motion.div variants={primaryVariants} className=" flex w-full items-start "></motion.div>
+              <motion.div
+                variants={primaryVariants}
+                className="flex w-full items-start"
+              ></motion.div>
 
               <input
                 value={t("Send")}
@@ -111,30 +117,30 @@ const ContactForm: React.FC = () => {
               />
             </form>
             {message && (
-          <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-              <div className="bg-white text-black p-6 rounded-lg shadow-lg w-96 h-32 flex flex-col items-center justify-center">
-              <button
-                onClick={() => setMessage(null)}
-                className="relative left-40 top-0 mb-4 font-bold  w-7 h-7 text-black text-3xl z-50"
-                aria-label="Close"
-              >
-                <IoCloseSharp />
-              </button>
-              <p className="text-lg font-semibold absolute top-3">{message}</p>
-              <button
-                onClick={() => setMessage(null)}
-                className="bg-[#764095] text-white px-4 py-2 mt-3 hover:bg-indigo-700"
-              >
-                {t("Close")}
-              </button>
-            </div>
-          </div>
-        )}
+              <div className="absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+                <div className="bg-white text-black p-6 rounded-lg shadow-lg w-96 h-32 flex flex-col items-center justify-center">
+                  <button
+                    onClick={() => setMessage(null)}
+                    className="relative left-40 top-0 mb-4 font-bold  w-7 h-7 text-black text-3xl z-50"
+                    aria-label="Close"
+                  >
+                    <IoCloseSharp />
+                  </button>
+                  <p className="text-lg font-semibold absolute top-3">
+                    {message}
+                  </p>
+                  <button
+                    onClick={() => setMessage(null)}
+                    className="bg-[#764095] text-white px-4 py-2 mt-3 hover:bg-indigo-700"
+                  >
+                    {t("Close")}
+                  </button>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       )}
-
-      
     </>
   );
 };
